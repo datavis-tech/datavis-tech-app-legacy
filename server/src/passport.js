f1c62c694e0dc6483610b9654d2681df39571209
curran <curran.kelleher@gmail.com>
2016-09-18 23:02:46 +0530

    Fat arrows everywhere

--- server/src/passport.js
+++ server/src/passport.js
@@ -4,16 +4,16 @@ import { GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET } from './config';
 
 const GitHubStrategy = passportGitHub.Strategy;
 
-passport.serializeUser(  function(user, done) { done(null, user); });
-passport.deserializeUser(function(user, done) { done(null, user); });
+passport.serializeUser(  (user, done) => done(null, user));
+passport.deserializeUser((user, done) => done(null, user));
 
 passport.use(new GitHubStrategy({
     clientID: GITHUB_CLIENT_ID,
     clientSecret: GITHUB_CLIENT_SECRET,
     callbackURL: "http://127.0.0.1:3000/auth/github/callback"
   },
-  function(accessToken, refreshToken, profile, cb) {
-    setTimeout(function (){
+  (accessToken, refreshToken, profile, cb) => {
+    setTimeout(() => {
       var user = {
         id: profile.id
       };
