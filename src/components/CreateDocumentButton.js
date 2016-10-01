1f9e173d9833061f3e96aa76287313a843fd51c7
curran <curran.kelleher@gmail.com>
2016-10-02 01:39:52 +0530

    Start on form

--- src/components/CreateDocumentButton.js
+++ src/components/CreateDocumentButton.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-import { Button, Modal } from 'react-bootstrap'
+import { Button, Modal, Form, FormGroup, FormControl, Col, ControlLabel } from 'react-bootstrap'
 
 class CreateDocumentButton extends Component {
 
@@ -26,10 +26,38 @@ class CreateDocumentButton extends Component {
 
         <Modal show={this.state.showModal} onHide={this.close}>
           <Modal.Header closeButton>
-            <Modal.Title>Modal heading</Modal.Title>
+            <Modal.Title>Create a new Document</Modal.Title>
           </Modal.Header>
           <Modal.Body>
-            <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula.</p>
+
+            <Form horizontal>
+
+              <FormGroup controlId="formHorizontalEmail">
+                <Col componentClass={ControlLabel} sm={2}>
+                  Title
+                </Col>
+                <Col sm={10}>
+                  <FormControl type="text" />
+                </Col>
+              </FormGroup>
+
+              <FormGroup controlId="formHorizontalEmail">
+                <Col componentClass={ControlLabel} sm={2}>
+                  Description
+                </Col>
+                <Col sm={10}>
+                  <FormControl type="text" />
+                </Col>
+              </FormGroup>
+
+              <FormGroup>
+                <Col smOffset={2} sm={10}>
+                  <Button type="submit">
+                    Sign in
+                  </Button>
+                </Col>
+              </FormGroup>
+            </Form>
           </Modal.Body>
           <Modal.Footer>
             <Button onClick={this.close}>Close</Button>
