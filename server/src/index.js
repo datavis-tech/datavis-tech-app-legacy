efb2e00e642d19125f48d73187071b27af7b9993
curran <curran.kelleher@gmail.com>
2016-10-01 21:03:19 +0530

    Decimate server code

--- server/src/index.js
+++ server/src/index.js
@@ -1,48 +1,12 @@
-import express from 'express';
-import session from './session';
-import passport from './passport';
+import ShareDB from 'sharedb'
+import { Connection } from 'sharedb/lib/client'
+import WebSocket, { Server } from 'ws'
+import WebSocketJSONStream from 'websocket-json-stream'
 
-const app = express();
-const port = 3000;
+const port = 8080
 
-app.use(session);
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-app.use(express.static('static'));
-
-// ***********************
-// * Auth-related routes *
-// ***********************
-app.get('/authcheck', (req, res) => {
-  if(req.user){
-    res.send('Logged in');
-  } else {
-    res.send('Not logged in');
-  }
-});
-
-app.get('/auth/github',
-  passport.authenticate('github'));
-
-app.get('/auth/github/callback', 
-  passport.authenticate('github', { failureRedirect: '/auth/failed' }),
-  (req, res) => res.redirect('/'));
-
-app.get('/auth/failed', (req, res) => {
-  res.send('Authentication failed');
-});
-
-app.get('/logout', (req, res) => {
-  req.logout();
-  res.redirect('/');
-});
-
-// ***********************
-// * End Auth-related routes *
-// ***********************
-
-
-app.listen(port);
-console.log('Listening on http://localhost:' + port);
+const sharedb = ShareDB()
+const wss = new WebSocketServer({ port })
+wss.on('connection', (ws, req) => {
+  sharedb.listen(new WebSocketJSONStream(ws))
+})
--- server/src/session.js
+++
@@ -1,8 +0,0 @@
-import session from 'express-session';
-import { SESSION_SECRET } from './config';
-
-export default session({
-  secret: SESSION_SECRET,
-  resave: false,
-  saveUninitialized: true
-});
