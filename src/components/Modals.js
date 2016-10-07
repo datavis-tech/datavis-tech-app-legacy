9401054281f3c533a978439019f690eacc395a87
curran <curran.kelleher@gmail.com>
2016-10-07 11:58:00 +0530

    Add modal content for create document UI

--- src/components/Modals.js
+++ src/components/Modals.js
@@ -2,16 +2,32 @@ import React from 'react'
 import { connect } from 'react-redux'
 import * as actionCreators from '../actionCreators'
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap'
+import { Form, FormGroup, Label, Input, Col } from 'reactstrap';
 
 const Modals = ({isOpen, hideModal}) => (
   <Modal isOpen={isOpen} toggle={hideModal}>
-    <ModalHeader toggle={hideModal}>Modal title</ModalHeader>
+    <ModalHeader toggle={hideModal}>Create Document</ModalHeader>
     <ModalBody>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      
+      <Form>
+        <FormGroup row>
+          <Label for="title" sm={2}>Title</Label>
+          <Col sm={10}>
+            <Input type="text" name="title" id="title" />
+          </Col>
+        </FormGroup>
+        <FormGroup row>
+          <Label for="description" sm={2}>Description</Label>
+          <Col sm={10}>
+            <Input type="textarea" name="description" id="description" />
+          </Col>
+        </FormGroup>
+      </Form>
+
     </ModalBody>
     <ModalFooter>
-      <Button color="primary" onClick={hideModal}>Do Something</Button>
       <Button color="secondary" onClick={hideModal}>Cancel</Button>
+      <Button color="primary" onClick={hideModal}>Create</Button>
     </ModalFooter>
   </Modal>
 )
