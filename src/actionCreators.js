d1b46dccd80871bf7d4c3e25782f8c93c2333fec
curran <curran.kelleher@gmail.com>
2016-10-07 20:53:16 +0530

    Stub document creation, adopt node-uuid

--- src/actionCreators.js
+++ src/actionCreators.js
@@ -1,2 +1,19 @@
-export const createDocument = (title, description) => ({
-})
+import connection from './connection'
+import uuid from 'node-uuid'
+
+export const collection = 'documents'
+
+export const createDocument = (title, description) => {
+  const initialValue = { title, description }
+  const id = uuid.v4()
+  const doc = connection.get(collection, id)
+  doc.create(initialValue, (err) => {
+    if(err){
+      //reject(err)
+    } else {
+      doc.whenNothingPending(() => {
+        console.log("Created document!" + id)
+      })
+    }
+  })
+}
