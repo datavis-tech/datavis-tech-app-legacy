febc646d1b7217b196dc2bd0edae6789950396d0
curran <curran.kelleher@gmail.com>
2016-10-07 14:24:28 +0530

    Filled in create document page

--- src/components/Create.js
+++ src/components/Create.js
@@ -3,7 +3,28 @@ import { connect } from 'react-redux'
 import * as actionCreators from '../actionCreators'
 
 const Create = () => (
-  <h1>Create</h1>
+  <div className="container">
+    <h1>Create</h1>
+    <form>
+      <div className="form-group row">
+        <label htmlFor="title" className="col-sm-2 col-form-label">Title</label>
+        <div className="col-sm-10">
+          <input type="text" className="form-control" id="title"/>
+        </div>
+      </div>
+      <div className="form-group row">
+        <label htmlFor="description" className="col-sm-2 col-form-label">Description</label>
+        <div className="col-sm-10">
+          <textarea className="form-control" id="description" rows="3"></textarea>
+        </div>
+      </div>
+      <div className="form-group row">
+        <div className="offset-sm-2 col-sm-10">
+          <button type="submit" className="btn btn-primary">Create</button>
+        </div>
+      </div>
+    </form>
+  </div>
 )
 
 export default connect(null, actionCreators)(Create)
