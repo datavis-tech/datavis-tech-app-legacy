0f3e59e1d1336d5b91d08306cc4933cf5629a928
curran <curran.kelleher@gmail.com>
2016-10-07 14:03:11 +0530

    Get nav links working

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
-import CreateButton from './components/CreateButton'
+import { Link } from 'react-router'
 
 const Navbar = () => (
   <nav className="navbar navbar-light bg-faded">
-    <a className="navbar-brand" href="#">Navbar</a>
+    <a className="navbar-brand" href="#">Datavis.tech</a>
     <ul className="nav navbar-nav">
       <li className="nav-item active">
-        <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+        <Link to="/">
+          <a className="nav-link">Home</a>
+        </Link>
       </li>
       <li className="nav-item">
         <a className="nav-link" href="#">Features</a>
@@ -19,7 +21,11 @@ const Navbar = () => (
       </li>
     </ul>
     <form className="form-inline pull-xs-right">
-      <CreateButton/>
+      <Link to="/create">
+        <button type="button" className="btn btn-primary">
+          Create
+        </button>
+      </Link>
     </form>
   </nav>
 )
--- src/components/CreateButton.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-const CreateButton = () => (
-  <button type="button" className="btn btn-primary">
-    Create
-  </button>
-)
-export default CreateButton
