e4421666c2517621e3c0919b7f78d36bea9cdae2
curran <curran.kelleher@gmail.com>
2016-10-08 12:45:17 +0530

    Adopt enzyme, add first actual test

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import {shallow} from 'enzyme';
 import App from './App'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
+it('renders a .container-fluid', () => {
+  const wrapper = shallow(<App/>)
+  expect(wrapper.find('.container-fluid').length).toBe(1)
 })
