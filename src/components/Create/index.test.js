714af28055b8faa231214c6d81d4ba7fb0c93afc
curran <curran.kelleher@gmail.com>
2016-10-08 21:45:34 +0530

    Add more detailed test

--- src/components/Create/index.test.js
+++ src/components/Create/index.test.js
@@ -29,8 +29,35 @@ it('renders a button', () => {
 
 it('validates against empty title and provides feedback', () => {
   const div = document.createElement('div')
-  ReactDOM.render(<Create />, div)
+  let createDocumentCalled = false
+  ReactDOM.render(<Create createDocument={ () => {
+    createDocumentCalled = true
+  }}/>, div)
   div.querySelector('button').dispatchEvent(new Event('click'))
   expect(div.querySelectorAll('.title-feedback').length).toBe(1)
   expect(div.querySelector('.title-feedback').textContent).toBe(titleFeedbackText)
+  expect(createDocumentCalled).toBe(false)
 })
+
+//it('calls createDocument when title is present', (done) => {
+//  const div = document.createElement('div')
+//  let createDocumentCalled = false
+//  ReactDOM.render(<Create createDocument={ ({title}) => {
+//    createDocumentCalled = true
+//  }}/>, div)
+//  const title = div.querySelector('#title')
+//  title.value = 'My Title'
+//  title.dispatchEvent(new Event('change', {id:'title', value:'foo'}))
+//  setTimeout(() =>{
+//    console.log("foo")
+//    done()
+//  }, 1000)
+//
+//  var evt = document.createEvent("HTMLEvents");
+//  evt.initEvent("change", false, true);
+//  title.dispatchEvent(evt);
+//
+//  //div.querySelector('button').dispatchEvent(new Event('click'))
+//  //expect(div.querySelectorAll('.title-feedback').length).toBe(0)
+//  //expect(createDocumentCalled).toBe(true)
+//})
