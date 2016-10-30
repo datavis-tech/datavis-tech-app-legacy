d09494fab426de0dc1019ba2fe4335809593d40c
curran <curran.kelleher@gmail.com>
2016-10-30 12:03:51 +0530

    Adopt Express and Express-session

--- src/server/config.js
+++ src/server/config.js
@@ -1,2 +1,5 @@
 // The port of the WebSocket server.
 export const port = 8080
+
+// The secret used with express-session.
+export const SESSION_SECRET = "kfn854j3k48emvdhjks85ur93jd29"
--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,30 @@
 import ShareDB from 'sharedb'
-import { Server } from 'ws'
+import { Server as WebSocketServer } from 'ws'
 import JSONStream from 'websocket-json-stream'
-import { port } from './config'
+import express from 'express'
+import session from 'express-session'
+import http from 'http'
+
+import {
+  port,
+//  SESSION_SECRET
+} from './config'
+
+const app = express()
+const server = http.createServer(app)
+const wss = new WebSocketServer({server})
 
 const sharedb = ShareDB()
-new Server({ port }).on('connection', (ws, req) => {
+
+wss.on('connection', (ws, req) =>
   sharedb.listen(new JSONStream(ws))
-})
+)
+
+// TODO connect this to Express
+//session({
+//  secret: SESSION_SECRET,
+//  resave: false,
+//  saveUninitialized: true
+//});
+
+server.listen(port)
