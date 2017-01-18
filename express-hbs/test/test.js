82e77080f3e8c7a84518d921e223bcb85686b1c4
curran <curran.kelleher@gmail.com>
2017-01-18 20:42:16 +0530

    Add test for existence of create form

--- express-hbs/test/test.js
+++ express-hbs/test/test.js
@@ -1,11 +1,18 @@
-casper.test.begin('Home Page', 1, function(test) {
+casper.test.begin('Home Page', 2, function(test) {
   var root = 'http://localhost:3000/';
 
   casper.start(root, function() {
     this.echo(this.getTitle());
-    test.assertEquals(this.getTitle(), 'Datavis Tech');
+    test.assertTitle('Datavis Tech', 'Title is correct');
+    test.assertExists('form[action="create"]', 'Create form is found');
     test.done();
-  }).run(function() {
+  });
+
+//this.fill('form[action="/search"]', {
+//  q: "casperjs"
+//}, true);
+  
+  casper.run(function() {
     test.done();
   });
 
