decb3869e9a1bd0b73fc28608d8f7499e2d7d494
curran <curran.kelleher@gmail.com>
2017-01-18 21:00:43 +0530

    Add tests for rendered title and description

--- express-hbs/test/test.js
+++ express-hbs/test/test.js
@@ -1,11 +1,12 @@
-casper.test.begin('Home Page', 3, function(test) {
-  var root = 'http://localhost:3000/';
+casper.test.begin('Home Page', 5, function(test) {
 
-  casper.start(root, function() {
-    this.echo(this.getTitle());
+  casper.start('http://localhost:3000/', function() {
+
     test.assertTitle('Datavis Tech', 'Title is correct');
+
     test.assertExists('form[action="create"]', 'Create form is found');
 
+    // Submit the form to create a new document.
     this.fill('form[action="create"]', {
       title: "testTitle",
       description: "testDescription"
@@ -20,18 +21,21 @@ casper.test.begin('Home Page', 3, function(test) {
     // http://localhost:3000/b84819e8-04cd-45ca-aa4d-b5e47181da68
     var url = this.getCurrentUrl();
 
-    test.assertEqual(url.length, 58);
+    test.assertEqual(url.length, 58, "Document was created, redirect occurred");
 
     // Extract the id of the document.
     var id = url.substr(22);
 
-    console.log(url);
-    // Draws from http://docs.casperjs.org/en/latest/testing.html#testing
     //test.assertTitle("casperjs - Recherche Google", "google title is ok");
-    //test.assertUrlMatch(/q=casperjs/, "search term has been submitted");
-    //test.assertEval(function() {
-    //  return __utils__.findAll("h3.r").length >= 10;
-    //}, "google search for \"casperjs\" retrieves 10 or more results");
+
+    test.assertEval(function() {
+      return $('#doc-title').text() === 'testTitle';
+    }, 'Rendered document title matches');
+
+    test.assertEval(function() {
+      return $('#doc-description').text() === 'testDescription';
+    }, 'Rendered document title matches');
+
   });
   
   casper.run(function() {
