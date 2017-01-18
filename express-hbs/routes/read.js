675850e331be4122e567876d1f939d2b8285a078
curran <curran.kelleher@gmail.com>
2017-01-18 16:10:28 +0530

    Implement data fetching logic for read route

--- express-hbs/routes/read.js
+++ express-hbs/routes/read.js
@@ -8,17 +8,17 @@ module.exports = function (connection){
 
   router.get('/:id', function(req, res, next) {
     var id = req.params.id
-    //var doc = connection.get('documents', id);
+    var doc = connection.get('documents', id);
 
-    //var data = {
-    //  title: req.body.title,
-    //  description:req.body.description
-    //};
-
-    //doc.create(data, function(err) {
-    //  if(err) return next(err);
-    //});
-    res.send(JSON.stringify(req.params));
+    // Draws from https://github.com/share/sharedb/blob/master/examples/textarea/server.js
+    doc.fetch(function(err) {
+      if(err) return next(err);
+      if(doc.type === null) {
+        res.send("No such document");
+      } else {
+        res.send(JSON.stringify(doc.data));
+      }
+    });
   });
 
   return router;
