4aad1ac0c07c7062c6e45aa34afe2b33af9b3e4a
curran <curran.kelleher@gmail.com>
2017-01-18 20:26:48 +0530

    Got casper to run something

--- express-hbs/test/test.js
+++ express-hbs/test/test.js
@@ -1,8 +1,11 @@
-var assert = require('assert');
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal(-1, [1,2,3].indexOf(4));
-    });
-  });
+var casper = require('casper').create();
+
+casper.start('http://casperjs.org/', function() {
+  this.echo(this.getTitle());
 });
+
+casper.thenOpen('http://phantomjs.org', function() {
+  this.echo(this.getTitle());
+});
+
+casper.run();
