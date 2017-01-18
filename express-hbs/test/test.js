f19d867b44ac5da81984e962a341329a93f1aabc
curran <curran.kelleher@gmail.com>
2017-01-18 20:39:08 +0530

    Got Casper Test to run against local server

--- express-hbs/test/test.js
+++ express-hbs/test/test.js
@@ -1,11 +1,12 @@
-var casper = require('casper').create();
+casper.test.begin('Home Page', 1, function(test) {
+  var root = 'http://localhost:3000/';
 
-casper.start('http://casperjs.org/', function() {
-  this.echo(this.getTitle());
+  casper.start(root, function() {
+    this.echo(this.getTitle());
+    test.assertEquals(this.getTitle(), 'Datavis Tech');
+    test.done();
+  }).run(function() {
+    test.done();
+  });
+
 });
-
-casper.thenOpen('http://phantomjs.org', function() {
-  this.echo(this.getTitle());
-});
-
-casper.run();
