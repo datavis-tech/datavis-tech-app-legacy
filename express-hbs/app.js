661fce38e44f1581b833c66239dcc5b78323452f
curran <curran.kelleher@gmail.com>
2017-01-19 12:04:54 +0530

    Setup websocket backend infra

--- express-hbs/app.js
+++ express-hbs/app.js
@@ -3,6 +3,7 @@
 //
 // Curran Kelleher January 2017
 
+// Express-related
 var http = require('http');
 var express = require('express');
 var hbs = require('hbs');
@@ -11,8 +12,12 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+
+// ShareDB-related
 var ShareDB = require('sharedb');
 var ShareDBMingoMemory = require('sharedb-mingo-memory');
+var WebSocket = require('ws');
+var WebSocketJSONStream = require('websocket-json-stream');
 
 var routes = require('./routes');
 
@@ -63,6 +68,15 @@ function start(){
   app.set('port', port);
 
   var server = http.createServer(app);
+
+  // Connect any incoming WebSocket connection to ShareDB
+  // Draws from https://github.com/share/sharedb/blob/master/examples/counter/server.js
+  var wss = new WebSocket.Server({server: server});
+  wss.on('connection', function(ws, req) {
+    var stream = new WebSocketJSONStream(ws);
+    backend.listen(stream);
+  });
+
   server.listen(port);
   server.on('listening', function (){
     console.log('Listening at http://localhost:' + port);
