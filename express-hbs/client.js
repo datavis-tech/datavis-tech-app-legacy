3436719d002cdd216fe52eb6da3b8b8bbf59b63c
curran <curran.kelleher@gmail.com>
2017-01-19 13:11:09 +0530

    Adopt sharedb-string-binding, got editor working

--- express-hbs/client.js
+++ express-hbs/client.js
@@ -1,5 +1,6 @@
-// Draws from https://github.com/share/sharedb/blob/master/examples/counter/client.js
+// Draws from https://github.com/share/sharedb/blob/master/examples/textarea/client.js
 var sharedb = require('sharedb/lib/client');
+var StringBinding = require('sharedb-string-binding');
 
 // Open WebSocket connection to ShareDB server
 var socket = new WebSocket('ws://' + window.location.host);
@@ -13,6 +14,10 @@ var id = snapshot.id;
 var doc = connection.get('documents', id);
 doc.ingestSnapshot(snapshot, function (err){
   if(err) throw err;
+
+  var element = document.getElementById('title-input');
+  var binding = new StringBinding(element, doc, ['title']);
+  binding.setup();
 
   console.log("snapshot ingested");
   console.log(doc.data);
