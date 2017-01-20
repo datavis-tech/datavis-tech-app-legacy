abf7890df04422434168e77e68c93280c69149a7
curran <curran.kelleher@gmail.com>
2017-01-20 15:00:06 +0530

    Add description update test

--- express-hbs/test/update.js
+++ express-hbs/test/update.js
@@ -2,20 +2,27 @@ module.exports = function (id, callback){
 
   console.log('');
 
-  casper.test.begin('Update', 2, function(test) {
+  casper.test.begin('Update', 4, function(test) {
     casper.start('http://localhost:3000/' + id + '/edit', function() {
       test.assertTitle('testTitle', 'Page title matches');
     });
 
     // Trigger the change to be sent via WebSocket to ShareDB server.
     casper.thenEvaluate(function() {
-      var el = document.getElementById('title-input');
-      el.value = 'newTitle';
-      el.dispatchEvent(new Event('input'));
+      $('#title-input').val('newTitle')[0].dispatchEvent(new Event('input'));
+      $('#description-input').val('newDescription')[0].dispatchEvent(new Event('input'));
     });
 
     casper.thenOpen('http://localhost:3000/' + id, function() {
       test.assertTitle('newTitle', 'Page title updated');
+
+      test.assertEval(function() {
+        return $('#doc-title').text() === 'newTitle';
+      }, 'Rendered document title matches');
+
+      test.assertEval(function() {
+        return $('#doc-description').text() === 'newDescription';
+      }, 'Rendered document description matches');
     });
     
     casper.run(function() {
