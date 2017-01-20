8a0a0ca8178c2fcab7b0927c6d1c8ef74990e8d2
curran <curran.kelleher@gmail.com>
2017-01-20 14:53:24 +0530

    Simplify update test

--- express-hbs/test/update.js
+++ express-hbs/test/update.js
@@ -7,29 +7,11 @@ module.exports = function (id, callback){
       test.assertTitle('testTitle', 'Page title matches');
     });
 
-    casper.waitFor(function() { 
-      return this.evaluate(function(){
-        return $('#title-input').val() === 'testTitle';
-      });
-    }, function() { 
-      this.echo('testTitle set'); 
-    });
-
+    // Trigger the change to be sent via WebSocket to ShareDB server.
     casper.thenEvaluate(function() {
-
       var el = document.getElementById('title-input');
       el.value = 'newTitle';
       el.dispatchEvent(new Event('input'));
-
-      setTimeout(function (){
-        window.titleSet = true;
-      }, 500);
-    });
-
-    casper.waitFor(function() { 
-      return this.getGlobal('titleSet'); 
-    }, function() { 
-      this.echo('titleSet flag has been set');
     });
 
     casper.thenOpen('http://localhost:3000/' + id, function() {
