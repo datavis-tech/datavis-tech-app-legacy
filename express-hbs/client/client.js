426475c3e4cdb014624d8266cf196a4008d413c5
curran <curran.kelleher@gmail.com>
2017-01-21 10:18:53 +0530

    Adopt d3

--- express-hbs/client/client.js
+++ express-hbs/client/client.js
@@ -1,33 +1,38 @@
 // Draws from https://github.com/share/sharedb/blob/master/examples/textarea/client.js
 var sharedb = require('sharedb/lib/client');
 var StringBinding = require('sharedb-string-binding');
+var d3 = require('d3-selection');
 
 // Open WebSocket connection to ShareDB server
 var socket = new WebSocket('ws://' + window.location.host);
 var connection = new sharedb.Connection(socket);
 
-window.connection = connection;
-
 // Parse the server-rendered JSON data bundle.
-var dataBundle = document.getElementById('data-bundle').textContent;
-var snapshot = JSON.parse(dataBundle);
-var id = snapshot.id;
+var dataBundle = d3.select('#data-bundle').text();
 
-var doc = connection.get('documents', id);
+update();
 
-function bindInput(id, property){
-  var element = document.getElementById(id);
-  var binding = new StringBinding(element, doc, [property]);
-  binding.setup();
+function update(){
+  var snapshot = JSON.parse(dataBundle);
+  var id = snapshot.id;
+
+  var doc = connection.get('documents', id);
+
+  function bindInput(id, property){
+    var element = document.getElementById(id);
+    var binding = new StringBinding(element, doc, [property]);
+    binding.setup();
+  }
+
+  doc.ingestSnapshot(snapshot, function (err){
+    if(err) throw err;
+
+    bindInput('title-input', 'title');
+    bindInput('description-input', 'description');
+
+    doc.subscribe(function (err){
+      if(err) throw err;
+    });
+  });
 }
 
-doc.ingestSnapshot(snapshot, function (err){
-  if(err) throw err;
-
-  bindInput('title-input', 'title');
-  bindInput('description-input', 'description');
-
-  doc.subscribe(function (err){
-    if(err) throw err;
-  });
-});
