803193e42464e07b31101f73d8c109a87ebd5e5d
curran <curran.kelleher@gmail.com>
2017-06-05 09:15:28 +0530

    Adopt nextjs + next-routes

---
+++ src/server.js
@@ -0,0 +1,20 @@
+// The main entry point for the server.
+// Draws from https://github.com/zeit/next.js/blob/v3-beta/examples/with-next-routes/server.js
+
+const { createServer } = require('http')
+const next = require('next')
+const routes = require('./routes')
+
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handler = routes.getRequestHandler(app)
+
+app
+  .prepare()
+  .then(() => {
+    createServer(handler)
+      .listen(3000, (err) => {
+        if (err) throw err
+        console.log('> Ready on http://localhost:3000')
+      })
+  })
