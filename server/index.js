dd8d30cfc6fe9781f8ced68b6aef3606bfabc4df
curran <curran.kelleher@gmail.com>
2017-06-05 09:52:59 +0530

    Adopt express

--- server/index.js
+++ server/index.js
@@ -1,18 +1,25 @@
 // The main entry point for the server.
-// Draws from https://github.com/zeit/next.js/blob/v3-beta/examples/with-next-routes/server.js
+// Draws from:
+//   https://github.com/zeit/next.js/blob/v3-beta/examples/with-next-routes/server.js
+//   https://github.com/zeit/next.js/blob/v3-beta/examples/custom-server-express/server.js
 
+const express = require('express')
 const { createServer } = require('http')
 const next = require('next')
 const routes = require('../routes')
 
 const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
-const handler = routes.getRequestHandler(app)
+const nextApp = next({ dev })
+const handler = routes.getRequestHandler(nextApp)
 
-app
+const expressApp = express()
+
+expressApp.get('*', handler)
+
+nextApp
   .prepare()
   .then(() => {
-    createServer(handler)
+    createServer(expressApp)
       .listen(3000, (err) => {
         if (err) throw err
         console.log('> Ready on http://localhost:3000')
