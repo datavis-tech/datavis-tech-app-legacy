d204ba9192c745cfaec152f5557470e059457742
curran <curran.kelleher@gmail.com>
2017-06-06 09:39:04 +0530

    Refactor

--- modules/globalClientStore.js
+++
@@ -1,4 +0,0 @@
-// This is a place where values from the server,
-// e.g. the data for the currently logged in user,
-// can be stored for use on client-side navigation.
-export default {}
--- modules/page.js
+++ modules/page.js
@@ -1,18 +1,28 @@
 // This module injects user data from the session into page props.
-// If server-rendered, the user data is stored globally
-// for later use by other pages afer client-side nagivation.
+// For this to work correctly, every Nextjs Page must be wrapped
+// by this, even if that page does not require access to user data.
+
+// If rendered on the server, the user data is extracted from the session,
+// passed to the browser, and stored globally in the browser
+// for later use by other pages after client-side nagivations.
 //
 // Draws from https://github.com/possibilities/next-github-auth/blob/60317b64f639cfd400ab8c932583341653cdb042/src/decorators/PageDecoratorInvariant.js
 
 import React from 'react'
-import GlobalClientStore from '../modules/globalClientStore'
+
+// This is a singleton variable on the browser side
+// used for storing the user data from the server.
+let globalUserData
 
 export default (Page) => {
   class WrappedPage extends React.Component {
 
     static async getInitialProps (pageContext) {
-      const user = process.browser ? GlobalClientStore.user : pageContext.req.user
 
+      // Get data about the currently logged in user.
+      const user = process.browser ? globalUserData : pageContext.req.user
+
+      // Support getInitialProps on the wrapped page.
       const pageProps = Page.getInitialProps
         ? await Page.getInitialProps(pageContext)
         : {}
@@ -22,8 +32,13 @@ export default (Page) => {
 
     constructor (props) {
       super(props)
+
+      // In the case that this page was server-rendered,
       if (process.browser) {
-        GlobalClientStore.user = props.user
+
+        // store the user data from the server into this singleton variable
+        // for use by other pages rendered on the client after a navigation.
+        globalUserData = props.user
       }
     }
 
