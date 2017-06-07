a5b675a7300ce624bb26a79691c44dcd65a66fae
curran <curran.kelleher@gmail.com>
2017-06-07 14:52:59 +0530

    Display title and description in view page

--- pages/view.js
+++ pages/view.js
@@ -1,21 +1,56 @@
 import React from 'react'
+import { Header } from 'semantic-ui-react'
 import Page from '../components/page'
 import Layout from '../components/layout'
+import connection from '../modules/shareDBConnection'
+import { DB_DOCUMENTS_COLLECTION} from '../modules/constants'
 
 class ViewPage extends React.Component {
+
   static async getInitialProps ({ query }) {
     return { id: query.id }
   }
 
+  constructor(props) {
+    super(props)
+    this.state = {
+      docInitialized: false
+    }
+    if(process.browser){
+      const doc = connection.get(DB_DOCUMENTS_COLLECTION, props.id)
+
+      const updateState = () => {
+        this.setState({
+          docInitialized: true,
+          title: doc.data.title,
+          description: doc.data.description,
+        })
+      }
+
+      doc.subscribe((err) => {
+        if (err) {
+          return console.error(err)
+        }
+        updateState()
+        doc.on('op', updateState)
+      })
+    }
+  }
+
   render () {
     const { id, user } = this.props
-
-    const userJSON = JSON.stringify(user, null, 2)
+    const { title, description } = this.state
 
     return (
       <Layout title={id + ' | Datavis.tech'} user={user}>
-        <h1>{id}</h1>
-        <pre>{userJSON}</pre>
+        {this.state.docInitialized ? (
+          <div>
+            <Header as='h1'>{title}</Header>
+            <p>{description}</p>
+          </div>
+        ) : (
+          <div>Loading...</div>
+        )}
       </Layout>
     )
   }
