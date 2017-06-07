214cddb6ee97c7abd5374cc4ef68ae9e14f46f4f
curran <curran.kelleher@gmail.com>
2017-06-07 10:53:34 +0530

    Add create route

--- routes.js
+++ routes.js
@@ -1,6 +1,7 @@
 const nextRoutes = require('next-routes')
 const routes = module.exports = nextRoutes()
 
+routes.add('create', '/create')
 routes.add('view', '/view/:id')
 routes.add('edit', '/edit/:id')
 routes.add('profile', '/:username')
