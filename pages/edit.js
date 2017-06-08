6e3e42684d230a47a089b3302aaaa4ba6a90f442
curran <curran.kelleher@gmail.com>
2017-06-08 07:41:51 +0530

    Adopt sharedb-string-binding. Got edit page to work.

--- pages/edit.js
+++ pages/edit.js
@@ -1,18 +1,43 @@
 import React from 'react'
+import { Form } from 'semantic-ui-react'
+import StringBinding from 'sharedb-string-binding'
 import Page from '../components/page'
 import Layout from '../components/layout'
-import { Form } from 'semantic-ui-react'
+import connection from '../modules/shareDBConnection'
+import { DB_DOCUMENTS_COLLECTION } from '../modules/constants'
 
 class EditPage extends React.Component {
   static async getInitialProps ({ query }) {
     return { id: query.id }
   }
 
+  constructor (props) {
+    super(props)
+    this.state = {
+      docInitialized: false
+    }
+    if (process.browser) {
+      this.doc = connection.get(DB_DOCUMENTS_COLLECTION, props.id)
+    }
+  }
+
+  componentDidMount () {
+    if (process.browser) {
+      // TODO cleanup on unmount
+      
+      this.doc.subscribe((err) => {
+        if (err) throw err;
+        new StringBinding(this.titleInput, this.doc, ['title']).setup()
+        new StringBinding(this.descriptionInput, this.doc, ['description']).setup()
+      })
+    }
+  }
+
   render () {
-    const { id, user } = this.props
+    const { user } = this.props
 
     return (
-      <Layout title={id + ' | Datavis.tech'} user={user}>
+      <Layout title={' (editing) | Datavis.tech'} user={user}>
         <Form>
           <Form.Field>
             <label>Title</label>
