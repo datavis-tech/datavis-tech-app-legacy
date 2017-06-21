bbf8d3bc4150cf12fed46884f9b168250726aaa8
curran <curran.kelleher@gmail.com>
2017-06-21 08:15:02 +0530

    Tweak margin on create page

--- pages/create.js
+++ pages/create.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Page from '../components/page'
 import Layout from '../components/layout'
-import { Form, Button } from 'semantic-ui-react'
+import { Form, Button, Container } from 'semantic-ui-react'
 import { Router } from '../routes'
 import { createDocument } from '../modules/shareDBGateway'
 
@@ -23,24 +23,26 @@ class CreatePage extends React.Component {
 
     return (
       <Layout title={'Create | Datavis.tech'} user={user}>
-        <h1>Create</h1>
-        <Form onSubmit={this.onSubmit.bind(this)}>
-          <Form.Field>
-            <label>Title</label>
-            <input
-              placeholder='Enter your title here.'
-              ref={(el) => { this.titleInput = el }}
-            />
-          </Form.Field>
-          <Form.Field>
-            <label>Description</label>
-            <input
-              placeholder='Enter your description here (optional).'
-              ref={(el) => { this.descriptionInput = el }}
-            />
-          </Form.Field>
-          <Button>Create</Button>
-        </Form>
+        <Container text>
+          <h1>Create</h1>
+          <Form onSubmit={this.onSubmit.bind(this)}>
+            <Form.Field>
+              <label>Title</label>
+              <input
+                placeholder='Enter your title here.'
+                ref={(el) => { this.titleInput = el }}
+              />
+            </Form.Field>
+            <Form.Field>
+              <label>Description</label>
+              <input
+                placeholder='Enter your description here (optional).'
+                ref={(el) => { this.descriptionInput = el }}
+              />
+            </Form.Field>
+            <Button>Create</Button>
+          </Form>
+        </Container>
       </Layout>
     )
   }
