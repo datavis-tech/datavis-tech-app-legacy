d159619066cc82c21a4f0d475fa244ca46179a1b
curran <curran.kelleher@gmail.com>
2017-06-30 10:42:55 +0530

    Truncate description. Closes #80

--- pages/profile/documentsList.js
+++ pages/profile/documentsList.js
@@ -1,13 +1,17 @@
 import { List } from 'semantic-ui-react'
 import { Link } from '../../routes'
 
+// Use only the first line of the description
+// as the tagline in the listing.
+const truncate = (description) => description.split('\n')[0];
+
 const DocumentPreview = ({id, data: { title, description }}) => (
   <List.Item key={id} >
     <List.Content>
       <Link route='view' params={{ id }}>
         <a>
           <List.Header>{title}</List.Header>
-          <List.Description>{description}</List.Description>
+          <List.Description>{truncate(description)}</List.Description>
         </a>
       </Link>
     </List.Content>
