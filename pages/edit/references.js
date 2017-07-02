70391d1e1bfab64ae26cde96b3161eee13a1d7b1
curran <curran.kelleher@gmail.com>
2017-07-02 22:57:21 +0530

    Reactify references ui

--- pages/edit/references.js
+++ pages/edit/references.js
@@ -1,33 +1,46 @@
 import { Table, Input } from 'semantic-ui-react'
 
-const References = () => (
-  <Table celled>
-    <Table.Header>
-      <Table.Row>
-        <Table.HeaderCell>File name</Table.HeaderCell>
-        <Table.HeaderCell>Document ID</Table.HeaderCell>
-      </Table.Row>
-    </Table.Header>
+class References extends React.Component {
 
-    <Table.Body>
-      <Table.Row>
-        <Table.Cell>
-          <Input transparent fluid placeholder='File name...' value='iris.csv' />
-        </Table.Cell>
-        <Table.Cell>
-          <Input transparent fluid placeholder='Document ID...' value='92f2f3a74cb84acc9f8527317d2f39f7'/>
-        </Table.Cell>
-      </Table.Row>
-      <Table.Row>
-        <Table.Cell>
-          <Input transparent fluid placeholder='File name...' />
-        </Table.Cell>
-        <Table.Cell>
-          <Input transparent fluid placeholder='Document ID...' />
-        </Table.Cell>
-      </Table.Row>
-    </Table.Body>
-  </Table>
-)
+  constructor (props) {
+    super(props)
+    this.state = {
+      references: [
+        {
+          fileName: 'iris.cskv',
+          id: '92f2f3a74cb84acc9f8527317d2f39f7'
+        }
+      ]
+    }
+  }
+
+  render () {
+    return (
+      <Table celled>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell>File name</Table.HeaderCell>
+            <Table.HeaderCell>Document ID</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+
+        <Table.Body>
+          {
+            this.state.references.map(({ fileName, id}, i) => (
+              <Table.Row key={id}>
+                <Table.Cell>
+                  <Input transparent fluid placeholder='File name...' value={fileName} ref={(el) => console.log(el)}/>
+                </Table.Cell>
+                <Table.Cell>
+                  <Input transparent fluid placeholder='Document ID...' value={id}/>
+                </Table.Cell>
+              </Table.Row>
+            ))
+          }
+        </Table.Body>
+      </Table>
+    )
+  }
+}
 
 export default References
