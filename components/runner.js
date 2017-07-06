118d449ae245fc2e37c15b14f03c55d25d66d6d6
curran <curran.kelleher@gmail.com>
2017-07-06 16:47:10 +0530

    Got rudimentary references working

--- components/runner.js
+++ components/runner.js
@@ -2,28 +2,66 @@
 
 import React from 'react'
 import magicSandbox from 'magic-sandbox'
+import { subscribeToDocument } from '../modules/shareDBGateway'
 
 class Runner extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
       template: props.doc.data.content,
+      files: {},
       allReferencesResolved: false
     }
   }
 
   componentDidMount () {
-    console.log('here')
-    setTimeout(() => {
-      this.setState({
-        allReferencesResolved: true,
-        files: {
-          'logData.js': {
-            content: 'console.log("Data!!")'
+    const references = this.props.doc.data.references || []
+    
+    if (process.browser) {
+      references.forEach(({ fileName, id}) => {
+        subscribeToDocument(id, (err, referenceDoc) => {
+          if (err) {
+            console.error(err)
           }
-        }
+
+          const newFile = {}
+          newFile[fileName] = {
+            content: referenceDoc.data.content
+          }
+          this.setState((state) => {
+            const newFiles = Object.assign(state.files, newFile)
+            return {
+              files: newFiles,
+              allReferencesResolved: Object.keys(newFile).length === references.length
+            }
+          })
+
+          //const updateState = () => {
+          //  this.setState({
+          //    docInitialized: true,
+          //    title: doc.data.title,
+          //    description: doc.data.description
+          //  })
+          //}
+          //doc.on('op', updateState)
+          //this.cleanupDoc = () => {
+          //  doc.destroy()
+          //  doc.removeListener('op', updateState)
+          //}
+        })
       })
-    }, 1000)
+    }
+
+    //setTimeout(() => {
+    //  this.setState({
+    //    allReferencesResolved: true,
+    //    files: {
+    //      'logData.js': {
+    //        content: 'console.log("Data!!")'
+    //      }
+    //    }
+    //  })
+    //}, 1000)
   }
 
   render () {
