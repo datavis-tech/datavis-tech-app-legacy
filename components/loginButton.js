13bf97f44ac8b28c14dbfbcd08176cb342750e0a
curran <curran.kelleher@gmail.com>
2017-07-22 10:58:32 +0530

    Provide feedback in login button. Closes #96

--- components/loginButton.js
+++ components/loginButton.js
@@ -1,8 +1,29 @@
+import React from 'react'
 import { Button } from 'semantic-ui-react'
 import { AUTH_PATH_GITHUB } from '../modules/constants'
-const LoginButton = () => (
-  <a href={AUTH_PATH_GITHUB}>
-    <Button>Sign in</Button>
-  </a>
-)
+
+class LoginButton extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      loading: false
+    }
+    this.handleClick = () => {
+      this.setState({
+        loading: true
+      })
+    }
+  }
+
+  render () {
+    const { loading } = this.state
+    return (
+      <a href={AUTH_PATH_GITHUB}>
+        <Button onClick={this.handleClick} disabled={loading} loading={loading}>
+          Sign in
+        </Button>
+      </a>
+    )
+  }
+}
 export default LoginButton
