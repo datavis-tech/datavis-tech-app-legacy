42b5791048d7f3064906f1aae8c35495519282bd
curran <curran.kelleher@gmail.com>
2017-10-03 21:44:43 +0530

    Lint

--- pages/index.js
+++ pages/index.js
@@ -1,59 +1,52 @@
-import {
-  Container,
-  Header,
-  Button,
-  Card,
-  Divider
-} from 'semantic-ui-react'
+import { Container, Header } from 'semantic-ui-react'
 import { Link } from '../routes'
 import Layout from '../components/layout'
 import Page from '../components/page'
 import Spacer from '../components/spacer'
-import VisualizationCard from './index/visualizationCard'
 import HugeLogo from '../components/hugeLogo'
 import { AUTH_PATH_GITHUB } from '../modules/constants'
 
-const visualizations = [
-  {
-    title: 'Campaign Finance Institute',
-    img: '/static/images/portfolio/cfi.JPG',
-    description: 'An interactive visualization for a database of campaign finance law, spanning all US States and every other year since 1996.',
-    links: [
-      {
-        icon: 'video play',
-        href: 'https://cfinst.github.io/',
-        title: 'Interactive Visualization'
-      },
-      {
-        icon: 'github',
-        href: 'https://github.com/cfinst/cfinst.github.io',
-        title: 'Source Code'
-      },
-      {
-        icon: 'youtube',
-        href: 'https://www.youtube.com/watch?v=hWAV2kkY1xk',
-        title: 'Video of Presentation'
-      }
-    ]
-  },
-  {
-    title: 'Josquin Ribbon',
-    img: '/static/images/portfolio/josquin.jpg',
-    description: 'An interactive visualization of Renaissance music curated and digitized by the Josquin Research Project at Stanford University.',
-    links: [
-      {
-        icon: 'video play',
-        href: 'http://ribbon.humdrum.org/',
-        title: 'Interactive Visualization'
-      },
-      {
-        icon: 'github',
-        href: 'https://github.com/sul-cidr/josquin-ribbon',
-        title: 'Source Code'
-      }
-    ]
-  }
-]
+// const visualizations = [
+//   {
+//     title: 'Campaign Finance Institute',
+//     img: '/static/images/portfolio/cfi.JPG',
+//     description: 'An interactive visualization for a database of campaign finance law, spanning all US States and every other year since 1996.',
+//     links: [
+//       {
+//         icon: 'video play',
+//         href: 'https://cfinst.github.io/',
+//         title: 'Interactive Visualization'
+//       },
+//       {
+//         icon: 'github',
+//         href: 'https://github.com/cfinst/cfinst.github.io',
+//         title: 'Source Code'
+//       },
+//       {
+//         icon: 'youtube',
+//         href: 'https://www.youtube.com/watch?v=hWAV2kkY1xk',
+//         title: 'Video of Presentation'
+//       }
+//     ]
+//   },
+//   {
+//     title: 'Josquin Ribbon',
+//     img: '/static/images/portfolio/josquin.jpg',
+//     description: 'An interactive visualization of Renaissance music curated and digitized by the Josquin Research Project at Stanford University.',
+//     links: [
+//       {
+//         icon: 'video play',
+//         href: 'http://ribbon.humdrum.org/',
+//         title: 'Interactive Visualization'
+//       },
+//       {
+//         icon: 'github',
+//         href: 'https://github.com/sul-cidr/josquin-ribbon',
+//         title: 'Source Code'
+//       }
+//     ]
+//   }
+// ]
 
 // const team = [
 //   {
@@ -138,16 +131,16 @@ const visualizations = [
 export default Page(({ user }) => (
   <Layout title='Datavis.tech' user={user}>
     <Container text>
-      <Spacer space="100px"/>
+      <Spacer space='100px' />
       <HugeLogo noLink />
-      <Spacer space="10px" />
+      <Spacer space='10px' />
       <Container textAlign='center'>
         <Header as='h1'>A collaborative data visualization platform.</Header>
       </Container>
-      <Spacer space="100px" />
+      <Spacer space='100px' />
       <p style={{fontSize: '1.5em'}}>You can use this site to publish datasets and create visualizations, collaborating with others in real time.</p>
       <p style={{fontSize: '1.5em'}}>To get started, <a href={AUTH_PATH_GITHUB}>log in via GitHub authentication</a> and take a look around, or have a look at some <Link route='profile' params={{ username: 'curran' }}><a>examples.</a></Link></p>
-      <Spacer space="250px"/>
+      <Spacer space='250px' />
     </Container>
   </Layout>
 ))
