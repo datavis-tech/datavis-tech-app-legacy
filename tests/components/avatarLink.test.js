1a56024a9e39cdd109fbbeb60fb204fe3e420eab
curran <curran.kelleher@gmail.com>
2017-10-17 16:42:16 +0530

    Lint tests

--- tests/components/avatarLink.test.js
+++ tests/components/avatarLink.test.js
@@ -1,28 +1,27 @@
-import React from 'react';
-import {shallow} from 'enzyme';
+import React from 'react'
+import {shallow} from 'enzyme'
 
-import {Image} from 'semantic-ui-react';
-import {Link} from '../../routes';
+import {Image} from 'semantic-ui-react'
+import {Link} from '../../routes'
 
-import AvatarLink from '../../components/avatarLink';
-
+import AvatarLink from '../../components/avatarLink'
 
 describe('avatar link', () => {
 
-  let props;
-  let sut;
+  let props
+  let sut
 
   beforeEach(() => {
-    props = {};
-    sut = shallow(<AvatarLink {...props}/>);
-  });
+    props = {}
+    sut = shallow(<AvatarLink {...props} />)
+  })
 
   it('should render nothing if user was not provided', () => {
     expect(sut.node).toBeNull()
-  });
+  })
 
   describe('if user was provided', () => {
-    let user;
+    let user
 
     beforeEach(() => {
       user = {
@@ -31,16 +30,16 @@ describe('avatar link', () => {
         _json: {
           avatar_url: String(Math.random())
         }
-      };
-      sut.setProps({user});
-    });
+      }
+      sut.setProps({user})
+    })
 
     it('should have a link', () => {
       expect(sut.find(Link).props()).toMatchObject({
         route: 'profile',
         params: {username: user.username}
-      });
-    });
+      })
+    })
 
     it('should have an image', () => {
       expect(sut.find(Image).props()).toMatchObject({
@@ -48,11 +47,11 @@ describe('avatar link', () => {
         src: `${user._json.avatar_url}&size=72`,
         inline: true
       })
-    });
+    })
 
     it('should display name', () => {
       expect(sut.find('a span').text()).toEqual(user.displayName)
-    });
+    })
   })
 
-});
+})
