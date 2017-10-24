063e49b2fff7e63517a72ce274cad22b29c13482
curran <curran.kelleher@gmail.com>
2017-10-24 19:14:16 +0530

    Add documentation, minor cleanup

--- tests/utils/fakeUser.js
+++ tests/utils/fakeUser.js
@@ -1,10 +1,9 @@
-
-export default function () {
-  return {
-    username: String(Math.random()),
-    displayName: String(Math.random()),
-    _json: {
-      avatar_url: String(Math.random())
-    }
+// This utility creates a fake "user" object that can be used
+// in any tests where a user object is required.
+export default () => ({
+  username: String(Math.random()),
+  displayName: String(Math.random()),
+  _json: {
+    avatar_url: String(Math.random())
   }
-}
+})
