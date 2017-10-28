c46ccdf517af84e0b6ade1057e676ff1f1b848e8
curran <curran.kelleher@gmail.com>
2017-10-28 15:04:40 +0530

    Got render props pattern working for data page

--- src/pages/data.js
+++ src/pages/data.js
@@ -3,11 +3,30 @@ import Page from '../components/page'
 import {ViewPage, ViewPageLayout} from '../components/viewPage'
 import DataViewer from '../components/dataViewer'
 
-export default Page(
-  ViewPage((props) => (
-    <ViewPageLayout
-      {...props}
-      Content={DataViewer}
-    />
-  ))
-)
+class DataViewPage extends React.Component {
+
+  // TODO refactor this into a common thing.
+  static async getInitialProps ({query}) {
+    return {
+      id: query.id
+    }
+  }
+
+  render () {
+    return (
+      <ViewPage id={this.props.id}>
+        { ({ownerProfile, doc}) => (
+          <ViewPageLayout
+            id={this.props.id}
+            user={this.props.user}
+            ownerProfile={ownerProfile}
+            doc={doc}
+            Content={DataViewer}
+          />
+        )}
+      </ViewPage>
+    )
+  }
+}
+
+export default Page(DataViewPage)
