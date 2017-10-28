32a974ba56f9d463fed5359fa3ff72c145511a55
curran <curran.kelleher@gmail.com>
2017-10-28 19:52:55 +0530

    Got first proper test to pass for data page

--- tests/pages/data.test.js
+++ tests/pages/data.test.js
@@ -2,38 +2,42 @@
 
 import React from 'react'
 import {render, shallow} from 'enzyme'
+import fakeDoc from '../utils/fakeDoc'
+import fakeUser from '../utils/fakeUser'
 
+const mockDoc = fakeDoc()
+const mockOwnerProfile = fakeUser()
+
+// Mock the ViewPage component with one that invokes children,
+// passing render prop with `ownerProfile` and `doc`.
 jest.mock('../../src/components/viewPage/viewPage', () => {
   return jest.fn(props => {
     return props.children({
-      ownerProfile: 'foo',
-      doc: 'bar'
+      ownerProfile: mockOwnerProfile,
+      doc: mockDoc
     })
   })
 })
 
+// Mock ViewPageLayout so we can test its props.
+jest.mock('../../src/components/viewPage/viewPageLayout', () => jest.fn(() => <div>Test</div>))
+
 import DataViewer from '../../src/components/dataViewer'
 import ViewPageDescription from '../../src/components/viewPage/viewPageDescription'
 import ViewPage from '../../src/components/viewPage/viewPage'
+import ViewPageLayout from '../../src/components/viewPage/viewPageLayout'
 import Data from '../../src/pages/data'
 
 describe('data page', () => {
 
   let sut
-  let DataPageLayout
-
   beforeEach(() => {
-    render(<Data />)
-    console.log(ViewPage.mock.calls)
-    DataPageLayout = ViewPage.mock.calls[0][0]
-    sut = shallow(<DataPageLayout />)
+    render(<Data id='foo'/>)
+    sut = ViewPageLayout.mock.calls[0][0]
   })
 
-  it('should render layout with data viewer as content and default description', () => {
-    expect(sut.props()).toMatchObject({
-      Description: ViewPageDescription
-    })
-    // children DataViewer,
+  it('should render layout with data viewer as content', () => {
+    expect(sut.Content).toEqual(DataViewer)
   })
 
 })
