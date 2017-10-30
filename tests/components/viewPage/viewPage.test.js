a7fcc1c98183bd7d32b0463f4c963a8e89746248
curran <curran.kelleher@gmail.com>
2017-10-30 19:12:56 +0530

    Got basic test for ViewPage working

--- tests/components/viewPage/viewPage.test.js
+++ tests/components/viewPage/viewPage.test.js
@@ -3,97 +3,39 @@ import {mount} from 'enzyme'
 
 import fakeDoc from '../../utils/fakeDoc'
 
-jest.mock('../../../src/db/subscribeToDocument')
-jest.mock('../../../src/db/createProfileQuery')
+const mockDoc = fakeDoc()
 
-import subscribeToDocument from '../../../src/db/subscribeToDocument'
-import createProfileQuery from '../../../src/db/createProfileQuery'
+jest.mock('../../../src/components/subscription', () => {
+  return jest.fn(props => {
+    return props.children({
+      docs: [mockDoc]
+    })
+  })
+})
+
 import ViewPage from '../../../src/components/viewPage/viewPage'
 
 describe('view page', () => {
 
-  let sut
-  let Component
-  let props
-
-  let id
-  let user
+  let childrenProps
 
   beforeEach(() => {
-    Component = props => (
+    const Component = props => (
       <ViewPage {...props}>
-        { () => <div>{String(Math.random())}</div> }
+        { props => {
+          childrenProps = props
+          return null
+        }}
       </ViewPage>
     )
-
-    id = Symbol('id')
-    user = Symbol('user')
-
-    props = {id, user}
-    sut = mount(<Component {...props} />)
+    mount(<Component />)
   })
 
-  describe('non browser env', () => {
-
-    it('should not subscribe to document', () => {
-      expect(subscribeToDocument).toHaveBeenCalledTimes(0)
+  it('should subscribe to document and owner', () => {
+    expect(childrenProps).toMatchObject({
+      doc: mockDoc,
+      ownerProfile: mockDoc.data
     })
-
-  })
-
-  describe('browser env', () => {
-
-    let callback
-    let docId
-
-    beforeAll(() => {
-      process.browser = true
-      sut.mount()
-    })
-
-    afterAll(() => {
-      process.browser = undefined
-    })
-
-    beforeEach(() => {
-      docId = subscribeToDocument.mock.calls[0][0]
-      callback = subscribeToDocument.mock.calls[0][1]
-    })
-
-    it('should subscribe to document', () => {
-      expect(docId).toEqual(id)
-    })
-
-    describe('on document changes', () => {
-
-      let doc
-
-      beforeEach(() => {
-        doc = fakeDoc()
-      })
-
-      it('should throw an error if any error occured', () => {
-        expect(() => {
-          callback(new Error(), doc)
-        }).toThrow()
-      })
-
-      describe('no errors', () => {
-
-        beforeEach(() => {
-          callback(null, doc)
-        })
-
-        it('should create profile', () => {
-          expect(createProfileQuery.mock.calls[0][0]).toEqual({
-            id: doc.data.owner
-          })
-        })
-
-      })
-
-    })
-
   })
 
 })
