f5a5c7cd3ee1373048188d14ad3f1ef73929fbed
curran <curran.kelleher@gmail.com>
2017-11-11 17:17:52 +0530

    Test cases with and without user

--- tests/components/navbar.test.js
+++ tests/components/navbar.test.js
@@ -1,16 +1,18 @@
 import React from 'react'
 import { shallow } from 'enzyme'
+import fakeUser from '../utils/fakeUser'
 
 import Navbar from '../../src/components/spacer'
 
 describe('spacer', () => {
-  let sut
 
-  beforeEach(() => {
-    sut = shallow(<Navbar />)
+  it(`should match snapshot without user`, () => {
+    const sut = shallow(<Navbar />)
+    expect(sut).toMatchSnapshot()
   })
 
-  it(`should match snapshot`, () => {
+  it(`should match snapshot with user`, () => {
+    const sut = shallow(<Navbar user={fakeUser}/>)
     expect(sut).toMatchSnapshot()
   })
 
