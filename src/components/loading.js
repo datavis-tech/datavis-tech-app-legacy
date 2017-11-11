388ae6aaff19432367591034231193fb86f9f4d2
curran <curran.kelleher@gmail.com>
2017-11-11 14:30:16 +0530

    Adopt Loader HOC Throughout Codebase. Closes #195

--- src/components/loading.js
+++
@@ -1,4 +0,0 @@
-import { Loader } from 'semantic-ui-react'
-
-// This component provides a spinning loading indicator.
-export default () => <Loader active inline='centered' />
--- src/pages/profile/profileBody.js
+++ src/pages/profile/profileBody.js
@@ -1,7 +1,7 @@
 import { Grid } from 'semantic-ui-react'
 import ProfileCard from './profileCard'
 import DocumentsList from './documentsList'
-import Loading from '../../components/loading'
+import Loader from '../../components/loader'
 
 const ProfileBody = (props) => {
   const {
@@ -11,23 +11,23 @@ const ProfileBody = (props) => {
     documentsLoading
   } = props
 
-  if (profileLoading) {
-    return <Loading />
-  }
-  if (!profile) {
-    return (
-      <div>User not found</div>
-    )
-  }
   return (
-    <Grid>
-      <Grid.Column width={4}>
-        <ProfileCard profile={profile} />
-      </Grid.Column>
-      <Grid.Column width={12}>
-        <DocumentsList documents={documents} documentsLoading={documentsLoading} />
-      </Grid.Column>
-    </Grid>
+    <Loader ready={!profileLoading}>
+      {
+        profile
+          ? (
+            <Grid>
+              <Grid.Column width={4}>
+                <ProfileCard profile={profile} />
+              </Grid.Column>
+              <Grid.Column width={12}>
+                <DocumentsList documents={documents} documentsLoading={documentsLoading} />
+              </Grid.Column>
+            </Grid>
+          )
+          : <div>User not found</div>
+      }
+    </Loader>
   )
 }
 
