ae1a37f37503535e41773ae0df732cbc35d3914e
curran <curran.kelleher@gmail.com>
2017-11-13 01:52:00 +0530

    Simplify ViewPage, make render props explicit in data and vis pages

--- src/components/viewPage/viewPage.js
+++ src/components/viewPage/viewPage.js
@@ -1,32 +1,18 @@
 import React from 'react'
-
 import DocumentSubscription from '../../db/subscriptions/documentSubscription'
-
 import Subscription from '../subscription'
 import Loader from '../loader'
 
-export default class VisViewPage extends React.Component {
-
-  constructor (props) {
-    super(props)
-    this.subscription = DocumentSubscription({id: props.id})
-  }
-
-  render () {
-
-    const {id, user, children} = this.props
-
-    return (
-      <Subscription subscription={this.subscription}>
-        {
-          ({data: doc, isReady}) => (
-            <Loader ready={isReady}>
-              {children({id, user, doc})}
-            </Loader>
-          )
-        }
-      </Subscription>
-    )
-  }
-
-}
+// Subscribes to the document with the given id,
+// and passes it as render prop to children.
+export default ({id, children}) => (
+  <Subscription subscription={DocumentSubscription({id})}>
+    {
+      ({data: doc, isReady}) => (
+        <Loader ready={isReady}>
+          {children(doc)}
+        </Loader>
+      )
+    }
+  </Subscription>
+)
