3aa0e2b7eb16d900c59e11a1cfad403a5c3cc259
curran <curran.kelleher@gmail.com>
2017-11-16 22:26:29 +0530

    First pass at business object for Visualization

---
+++ src/businessObjects/visualization.js
@@ -0,0 +1,45 @@
+import {
+  id,
+  hasData,
+  references,
+  referenceIds,
+  allReferencesLoaded,
+  title,
+  description,
+  content,
+  owner,
+  collaborators,
+  profile
+} from '../../src/db/accessors'
+
+import {
+  createDocument,
+  deleteDocument,
+  addCollaborator,
+  removeCollaborator,
+  addReference,
+  removeReference
+} from '../../src/db/actions'
+
+export const Visualization = shareDBDoc => ({
+  id: () => id(shareDBDoc),
+  hasData: () => hasData(shareDBDoc),
+  references: () => references(shareDBDoc),
+  referenceIds: () => referenceIds(shareDBDoc),
+  allReferencesLoaded: referenceDocs => allReferencesLoaded(shareDBDoc, referenceDocs),
+  title: () => title(shareDBDoc),
+  description: () => description(shareDBDoc),
+  content: () => content(shareDBDoc),
+  owner: () => owner(shareDBDoc),
+  collaborators: () => collaborators(shareDBDoc),
+  profile: () => profile(shareDBDoc),
+
+  delete: callback => deleteDocument(shareDBDoc, callback),
+  addCollaborator: () => addCollaborator(shareDBDoc),
+  removeCollaborator: () => removeCollaborator(shareDBDoc),
+  addReference: (fileName, id) => addReference(shareDBDoc, fileName, id),
+  removeReference: referenceIndex => removeReference(shareDBDoc, referenceIndex)
+})
+
+// TODO think this through
+// Visualization.create = () => createDocument({type: VIZ_TYPE})
