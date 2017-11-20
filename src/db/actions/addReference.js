1f24baf0a028ce73b22866cde6b3eb08fe179ca2
curran <curran.kelleher@gmail.com>
2017-11-21 01:44:34 +0530

    Introduce primitives for push and listDelete

--- src/db/actions/addReference.js
+++ src/db/actions/addReference.js
@@ -1,18 +1,14 @@
+import { push } from './primitives'
+
 // Adds the given reference (fileName and id)
 // to the given document.
-export const addReference = (doc, fileName = '', id = '') => {
-
-  // If references is undefined, then create an empty array.
-  if (!doc.data.references) {
-    doc.submitOp([{
-      p: ['references'],
-      oi: []
-    }])
-  }
-
-  // Push an empty reference object onto the references array.
-  doc.submitOp([{
-    p: ['references', doc.data.references.length],
-    li: { fileName, id }
-  }])
-}
+export const addReference = (shareDBDoc, fileName = '', id = '') => (
+  push({
+    shareDBDoc,
+    property: 'references',
+    item: {
+      fileName,
+      id
+    }
+  })
+)
