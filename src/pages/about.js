85440a1739bf98be3cb405c57c74e0c0cc522f45
Curran <curran.kelleher@gmail.com>
2017-12-27 15:21:51 -0500

    Lint

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,8 @@
-import { Header, Segment, Container } from 'semantic-ui-react'
+import { Header, Container } from 'semantic-ui-react'
 import Page from '../components/page'
-
 import Layout from '../components/layout'
-import Spacer from '../components/spacer'
-import HugeLogo from '../components/hugeLogo'
 
-//const visualizations = [
+// const visualizations = [
 //  {
 //    title: 'Campaign Finance Institute',
 //    img: '/static/images/portfolio/cfi.JPG',
@@ -45,7 +42,7 @@ import HugeLogo from '../components/hugeLogo'
 //      }
 //    ]
 //  }
-//]
+// ]
 
 // const team = [
 //   {
@@ -134,7 +131,7 @@ export default Page(({ user }) => (
       <Header as='h1'>About Datavis.tech</Header>
       <p> Datavis Tech Inc. provides services and products relating to data visualization design, implementation, and training. </p>
       <p> Datavis Tech INC was founded by Curran Kelleher in January 2016. For the first year, the corporation was a vehicle for data visualization consulting projects and freelance work. Since its inception, the idea of a platform for collaborative data visualization was brewing. The datavis.tech platform was prototyped many times throughout 2016 and 2017, and launched in its most basic form in June 2017.</p>
-    
+
       <p>At the moment, here's what you can do with Datavis.tech:</p>
 
       <ul>
@@ -147,8 +144,8 @@ export default Page(({ user }) => (
         <li>See the data behind any visualization</li>
       </ul>
 
-      <p>We are continually adding new features to the platform in a long-term effort to build the premiere platform for creating, hosting, and leveraging the full potential of interactive data visualizations on the Web. The platform is heavily inspired by <a href="https://bl.ocks.org">bl.ocks.org</a> and <a href="https://blockbuilder.org">Blockbuilder</a>, which have blossomed as tools for creating and hosting visualization examples. We want to go beyond what those services provide in terms of supporting collaboration and wide dissemination of interactive data visualizations. When data visualizations are first-class citizens of the Web, there are many many opportunities to better serve visualization creators and visualization consumers.</p>
-    
+      <p>We are continually adding new features to the platform in a long-term effort to build the premiere platform for creating, hosting, and leveraging the full potential of interactive data visualizations on the Web. The platform is heavily inspired by <a href='https://bl.ocks.org'>bl.ocks.org</a> and <a href='https://blockbuilder.org'>Blockbuilder</a>, which have blossomed as tools for creating and hosting visualization examples. We want to go beyond what those services provide in terms of supporting collaboration and wide dissemination of interactive data visualizations. When data visualizations are first-class citizens of the Web, there are many many opportunities to better serve visualization creators and visualization consumers.</p>
+
       <p>Here are some examples of people who could leverage datavis.tech:</p>
 
       <ul>
