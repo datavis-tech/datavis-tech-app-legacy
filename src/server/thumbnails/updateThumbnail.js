f0eb0c1df0c89a8d856caa68b9c2d7ed942e7c31
Curran Kelleher <curran.kelleher@gmail.com>
2018-01-22 12:24:45 +0000

    Remove accidentally added console.log from updateThumbnail.js

--- src/server/thumbnails/updateThumbnail.js
+++ src/server/thumbnails/updateThumbnail.js
@@ -2,8 +2,6 @@ const setThumbnail = require('../../db/actions/setThumbnail')
 const generateThumbnailBuffer = require('./generateThumbnailBuffer')
 
 module.exports = async (browser, sandbox, shareDbDoc) => {
-  console.log('updating thumbnail for ' + shareDbDoc.id)
-
   const html = await sandbox({id: shareDbDoc.id})
   const page = await browser.newPage()
   const thumbnailBuffer = await generateThumbnailBuffer(page, html)
