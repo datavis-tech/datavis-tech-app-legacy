a067c2f35f699a0c7a25873229138de9fc4bbaff
Curran <curran.kelleher@gmail.com>
2018-02-01 17:22:18 +0530

    Use comma format for view count

--- src/pages/views/slots/viewCount.js
+++ src/pages/views/slots/viewCount.js
@@ -1,9 +1,16 @@
 import { Statistic } from 'semantic-ui-react'
+import { format } from 'd3-format'
+
+const commaFormat = format(',')
 
 // TODO: test
 export default ({ viewCount }) => (
   <Statistic horizontal size='small'>
-    <Statistic.Value>{viewCount}</Statistic.Value>
-    <Statistic.Label>Views</Statistic.Label>
+    <Statistic.Value>
+      {commaFormat(viewCount)}
+    </Statistic.Value>
+    <Statistic.Label>
+      Views
+    </Statistic.Label>
   </Statistic>
 )
