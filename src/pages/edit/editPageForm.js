1859f49f69c06998cb15dbf6056bf21b33ea4132
Curran <curran.kelleher@gmail.com>
2018-02-02 23:12:56 +0530

    Change 'content' to 'index.html' in Vis Edit Page. Closes #286

--- src/pages/edit/editPageForm.js
+++ src/pages/edit/editPageForm.js
@@ -3,6 +3,9 @@ import { Form, Grid, Button } from 'semantic-ui-react'
 import { Link } from '../../routes'
 import CodeMirrorBinding from '../../components/codeMirrorBinding'
 import StringBinding from '../../components/stringBinding'
+import { VIS_DOC_TYPE } from '../../constants'
+
+const contentLabel = type => type === VIS_DOC_TYPE ? 'index.html' : 'Content'
 
 // The Form in the body of the page.
 export default (
@@ -97,7 +100,7 @@ export default (
     }
 
     <Form.Field>
-      <label>Content</label>
+      <label>{contentLabel(document.type)}</label>
       <CodeMirrorBinding
         doc={__shareDbDoc}
         path={['content']}
