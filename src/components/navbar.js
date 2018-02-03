1b2c3395d41999ecbd46cae48d3abe902abc0151
Curran <curran.kelleher@gmail.com>
2018-02-04 00:13:11 +0530

    Fix nav logo link behavior

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -29,11 +29,13 @@ const CreateMenu = ({user}) => {
 // which includes the logo, create button, and login control.
 const Navbar = ({user}) => (
   <Menu secondary>
-    <Link href='/'>
-      <Menu.Item header >
-        <Image height='36px' verticalAlign='middle' src='/static/images/Logo_Nav.png' />
-      </Menu.Item>
-    </Link>
+    <Menu.Item>
+      <Link route='/'>
+        <a>
+          <Image height='36px' src='/static/images/Logo_Nav.png' />
+        </a>
+      </Link>
+    </Menu.Item>
     <Menu.Menu position='right'>
       <CreateMenu user={user} />
       <LoginControl user={user} />
