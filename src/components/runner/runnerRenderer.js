a99a52fa0126b184c54f8b5e67254b9f20e589a6
Curran <curran.kelleher@gmail.com>
2018-02-04 17:42:49 +0530

    Responsive layout. Closes #308

--- src/components/runner/runnerRenderer.js
+++ src/components/runner/runnerRenderer.js
@@ -3,7 +3,7 @@ import magicSandbox from 'magic-sandbox'
 // This "dumb" component runs the code for a visualization in an iframe.
 export default ({template, files}) => (
   <iframe
-    width='960'
+    width='100%'
     height='500'
     scrolling='no'
     style={{ border: 'solid 1px #ddd' }}
