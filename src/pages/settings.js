2b6ad40d34c85a170daf9fca025a2002a757f84f
Curran <curran.kelleher@gmail.com>
2018-02-05 13:20:33 +0530

    Iterate Stripe checkout params

--- src/pages/settings.js
+++ src/pages/settings.js
@@ -8,8 +8,13 @@ export default Page(({ user }) => (
   <Layout title='Settings | Datavis.tech' user={user} hideFeedback >
     <Header as='h1'>Settings</Header>
     <p>You are currently on the <strong>Free</strong> plan.</p>
-    <Button primary>Upgrade to the Early Adopter Plan</Button>
     <StripeCheckout
+      label='Upgrade to the Early Adopter Plan'
+      name='Datavis Tech INC.'
+      description='Early Adopter Plan - $5/mo'
+      image='/static/images/logo/Logo_Icon_128px.png'
+      amount={500}
+      currency='USD'
       token={token => console.log(token)}
       stripeKey='pk_test_Y4thsPih1A0NNySQzyX7DQEi'
     />
