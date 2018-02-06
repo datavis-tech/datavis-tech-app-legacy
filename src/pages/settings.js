d26a31537c0e5f8d6d50bb8cdfec965d65772189
Curran <curran.kelleher@gmail.com>
2018-02-07 01:28:39 +0530

    Pass Stripe token to server

--- src/pages/settings.js
+++ src/pages/settings.js
@@ -4,19 +4,39 @@ import Page from '../components/page'
 import Layout from '../components/layout'
 import StripeCheckout from 'react-stripe-checkout'
 
-export default Page(({ user }) => (
-  <Layout title='Settings | Datavis.tech' user={user} hideFeedback >
-    <Header as='h1'>Settings</Header>
-    <p>You are currently on the <strong>Free</strong> plan.</p>
-    <StripeCheckout
-      label='Upgrade to the Early Adopter Plan'
-      name='Datavis Tech INC.'
-      description='Early Adopter Plan - $5/mo'
-      image='/static/images/logo/Logo_Icon_128px.png'
-      amount={500}
-      currency='USD'
-      token={token => console.log(token)}
-      stripeKey='pk_test_Y4thsPih1A0NNySQzyX7DQEi'
-    />
-  </Layout>
-))
+const onToken = token => {
+  fetch('/stripe/earlyAdopterUpgrade', {
+    method: 'POST',
+    body: JSON.stringify(token),
+    headers: new Headers({
+      'Content-Type': 'application/json'
+    })
+  }).then(response => {
+    response.json().then(data => {
+      console.log(data)
+    });
+  });
+}
+
+export default Page(({ user }) => {
+  if(global.fetch){
+    onToken({testing:'shiot'})
+  }
+
+  return (
+    <Layout title='Settings | Datavis.tech' user={user} hideFeedback >
+      <Header as='h1'>Settings</Header>
+      <p>You are currently on the <strong>Free</strong> plan.</p>
+      <StripeCheckout
+        label='Upgrade to the Early Adopter Plan'
+        name='Datavis Tech INC.'
+        description='Early Adopter Plan - $5/mo'
+        image='/static/images/logo/Logo_Icon_128px.png'
+        amount={399}
+        currency='USD'
+        token={onToken}
+        stripeKey='pk_test_Y4thsPih1A0NNySQzyX7DQEi'
+      />
+    </Layout>
+  )
+})
