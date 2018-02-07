de5a00e5b01066f29d5e2ff84b402df468e6243f
Curran <curran.kelleher@gmail.com>
2018-02-07 10:42:46 +0530

    Add missing file

---
+++ src/server/stripeIntegration.js
@@ -0,0 +1,15 @@
+const config = require('../config.js')
+//const stripe = require('stripe')('pk_test_Y4thsPih1A0NNySQzyX7DQEi')
+
+module.exports = (expressApp) => {
+  expressApp.post('/stripe/earlyAdopterUpgrade', (req, res) => {
+    const { email, id } = req.body
+    console.log(email, id)
+    // TODO create customer
+    // TODO create subscription
+    // TODO update user in DB with plan field changed
+    res.send({
+      foo: 'bar'
+    })
+  })
+}
