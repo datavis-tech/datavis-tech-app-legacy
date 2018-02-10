daf40c1c15234c515788a99e46dc211ce56f000a
Curran <curran.kelleher@gmail.com>
2018-02-10 14:24:59 +0530

    Add Stripe webhooks

--- src/server/stripeIntegration.js
+++ src/server/stripeIntegration.js
@@ -2,6 +2,9 @@ const config = require('../config.js')
 const stripe = require('stripe')(config.stripeSecretKey)
 
 module.exports = (expressApp) => {
+
+  // This gets invoked after the user clicks "Upgrade"
+  // and enters their credit card details.
   expressApp.post('/stripe/earlyAdopterUpgrade', (req, res) => {
     const { email, id } = req.body
     stripe.customers.create({
@@ -13,10 +16,33 @@ module.exports = (expressApp) => {
         plan: 'early-adopter'
       }]
     })).then(subscription => {
-      // TODO update User entry with plan flag set
+      // User plan will be updated in DB via Webhook.
       res.send({ success: true })
     }).catch(error => {
       res.send({ error })
     })
+  })
+
+  // This gets invoked by Stripe.
+  // See https://stripe.com/docs/webhooks
+  expressApp.post('/stripe/webhook', (req, res) => {
+    // TODO verify signature https://stripe.com/docs/webhooks/signatures
+    console.log(req.body)
+    const type = req.body.type
+    switch (type) {
+      case 'customer.subscription.created':
+        // TODO update subscriptionPlan to 'earlyAdopter' in User entry in DB
+        break
+      case 'customer.subscription.deleted':
+        // TODO update subscriptionPlan to 'null' in User entry in DB
+        break
+      case 'invoice.payment_succeeded':
+        // TODO update subscriptionStatus to 'active' in User entry in DB
+        break
+      case 'invoice.payment_failed':
+        // TODO update subscriptionStatus to 'pastDue' in User entry in DB
+        break
+    }
+    res.send(200)
   })
 }
