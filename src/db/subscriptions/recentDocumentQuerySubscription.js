50e93624c4b5840e3e473a858ca8e361fdc3961e
Curran <curran.kelleher@gmail.com>
2018-02-13 00:03:47 +0530

    Use ctime for sorting. Restrict query to vis type

--- src/db/subscriptions/recentDocumentQuerySubscription.js
+++ src/db/subscriptions/recentDocumentQuerySubscription.js
@@ -1,11 +1,12 @@
-import { DB_DOCUMENTS_COLLECTION } from '../../constants'
+import { DB_DOCUMENTS_COLLECTION, VIS_DOC_TYPE } from '../../constants'
 import BaseQuerySubscription from './baseQuerySubscription'
 
 // TODO: test
 export default () => BaseQuerySubscription(
   {
+    type: VIS_DOC_TYPE,
     viewCount: {$gte: 10},
-    $sort: {'_m.mtime': -1}
+    $sort: {'_m.ctime': -1}
   },
   DB_DOCUMENTS_COLLECTION
 )
