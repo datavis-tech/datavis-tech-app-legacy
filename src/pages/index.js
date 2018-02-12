2053be8793c1b38ca99ce32954766610374faed7
Curran <curran.kelleher@gmail.com>
2018-02-13 00:30:01 +0530

    Minor iteration on home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Container, Header } from 'semantic-ui-react'
-import { Link } from '../routes'
 import RecentVisualizationsQuerySubscription from '../db/subscriptions/recentDocumentQuerySubscription'
 import { serializeDocument } from '../db/serializers'
 import Subscription from '../components/subscription'
@@ -10,7 +9,6 @@ import Loader from '../components/loader'
 import Spacer from '../components/spacer'
 import HugeLogo from '../components/hugeLogo'
 import DocumentPreviewList from '../pages/views/slots/documentPreviewList'
-import { AUTH_PATH_GITHUB } from '../constants'
 
 // TODO: test
 export default Page(({ user }) => (
@@ -22,10 +20,9 @@ export default Page(({ user }) => (
       <Container textAlign='center'>
         <Header as='h1'>A collaborative data visualization platform.</Header>
       </Container>
-      <Spacer space='100px' />
-      <p style={{fontSize: '1.5em'}}>You can use this site to publish datasets and create visualizations, collaborating with others in real time.</p>
-      <p style={{fontSize: '1.5em'}}>To get started, <a href={AUTH_PATH_GITHUB}>log in via GitHub authentication</a> and take a look around, or have a look at some <Link route='profile' params={{ username: 'curran' }}><a>examples.</a></Link></p>
-      <Spacer space='20px' />
+      <Spacer space='50px' />
+      <p style={{fontSize: '1.5em', textAlign: 'center'}}>You can use this site to publish datasets and create visualizations, collaborating with others in real time.</p>
+      <Spacer space='50px' />
       <Container>
         <Subscription subscription={RecentVisualizationsQuerySubscription()} >
           {
