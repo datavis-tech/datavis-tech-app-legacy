691a55a8c951c3a2538b5fcb7c42d9e761b3c4d7
Curran <curran.kelleher@gmail.com>
2018-02-18 17:34:38 +0530

    Redesign profile page. Closes #310

--- src/pages/profile/profileCard.js
+++ src/pages/profile/profileCard.js
@@ -1,14 +1,21 @@
 // This defines the card element that contains the
 // profile picture, display name, and username.
-import { Card, Image } from 'semantic-ui-react'
+import { Image, Header } from 'semantic-ui-react'
+import { avatarURL } from '../../db/accessors'
 
 // TODO refactor to use accessors
 export default ({ profile }) => (
-  <Card>
-    <Image src={profile._json.avatar_url} />
-    <Card.Content>
-      <Card.Header>{profile.displayName}</Card.Header>
-      <Card.Meta>{profile.username}</Card.Meta>
-    </Card.Content>
-  </Card>
+  <div>
+    <Image circular bordered size='small' src={avatarURL(profile)} />
+    <div style={{marginTop: '0.5em'}}>
+      <Header>
+        <Header.Content>
+          {profile.displayName}
+          <Header.Subheader>
+            {profile.username}
+          </Header.Subheader>
+        </Header.Content>
+      </Header>
+    </div>
+  </div>
 )
