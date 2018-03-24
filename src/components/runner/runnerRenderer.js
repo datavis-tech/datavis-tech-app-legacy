f30657a2ace1e142779a9f0b6e1512e7fd0b8379
Curran <curran.kelleher@gmail.com>
2018-03-24 20:49:30 +0530

    Sketch of flicker-free runner solution

--- src/components/runner/runnerRenderer.js
+++ src/components/runner/runnerRenderer.js
@@ -1,11 +1,44 @@
+import React, { Component } from 'react'
 import magicSandbox from 'magic-sandbox'
 
 // This "dumb" component runs the code for a visualization in an iframe.
-export default ({template, files}) => (
-  <iframe
-    width='100%'
-    height='500'
-    scrolling='no'
-    srcDoc={magicSandbox(template, files)}
-  />
-)
+export default class RunnerRenderer extends Component {
+  constructor (props) {
+    super(props)
+    const updateInterval = props.updateInterval || 300
+
+    this.state = {
+    };
+
+    this.interval = setInterval(() => {
+      if (this.needsBufferSwap) {
+        swapBuffers()
+        this.needsBufferSwap = false
+      }
+      if (this.contentChanged) {
+        renderBackBuffer()
+        this.needsBufferSwap = true
+      }
+    }, updateInterval)
+  }
+
+  componentWillReceiveProps (props) {
+
+  }
+
+  componentWillUnmount () {
+    clearInterval(this.interval)
+  }
+
+  render(){
+    const {template, files} = this.props
+    return (
+      <iframe
+        width='100%'
+        height='500'
+        scrolling='no'
+        srcDoc={magicSandbox(template, files)}
+      />
+    )
+  }
+}
