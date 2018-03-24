ce285e7a5759ff1213723469ccf9d9b4e5782895
Curran <curran.kelleher@gmail.com>
2018-03-24 21:07:36 +0530

    More sketching of flicker-free runner solution

--- src/components/runner/runnerRenderer.js
+++ src/components/runner/runnerRenderer.js
@@ -1,29 +1,56 @@
 import React, { Component } from 'react'
 import magicSandbox from 'magic-sandbox'
 
+// Z Indices for layering iframe buffers.
+const BACK = 4;
+const FRONT = 5;
+
 // This "dumb" component runs the code for a visualization in an iframe.
 export default class RunnerRenderer extends Component {
   constructor (props) {
     super(props)
     const updateInterval = props.updateInterval || 300
 
+    this.contentChanged = true
+
     this.state = {
+      bufferA: {
+        zIndex: BACK,
+        srcDoc: ''
+      },
+      bufferB: {
+        zIndex: FRONT,
+        srcDoc: ''
+      }
     };
 
     this.interval = setInterval(() => {
       if (this.needsBufferSwap) {
-        swapBuffers()
+        this.swapBuffers()
         this.needsBufferSwap = false
       }
       if (this.contentChanged) {
-        renderBackBuffer()
+        this.updateBackBuffer()
         this.needsBufferSwap = true
       }
     }, updateInterval)
   }
 
-  componentWillReceiveProps (props) {
+  swapBuffers(){
+  }
 
+  updateBackBuffer () {
+    const {template, files} = this.props
+    const backBufferName = this.state.buffers.bufferA.zIndex === BACK ? 'bufferA' : 'bufferB';
+    this.setState(Object.assign({}, this.state, {
+      [backBufferName]: {
+        zIndex: this.state.buffers[backBufferName].zIndex,
+        srcDoc: magicSandbox(template, files)
+      }
+    }))
+  }
+
+  componentWillReceiveProps (props) {
   }
 
   componentWillUnmount () {
@@ -31,14 +58,14 @@ export default class RunnerRenderer extends Component {
   }
 
   render(){
-    const {template, files} = this.props
-    return (
+    return Object.values(this.state.buffers).map(({zIndex, srcDoc}) => (
       <iframe
+        style={{zIndex}}
         width='100%'
         height='500'
         scrolling='no'
-        srcDoc={magicSandbox(template, files)}
+        srcDoc={srcDoc}
       />
-    )
+    ))
   }
 }
