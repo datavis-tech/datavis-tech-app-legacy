f4cae828a87dae5b98a95b21b4b5ffd19d60fe3e
Curran <curran.kelleher@gmail.com>
2018-04-24 01:48:46 +0530

    Add tech docs to home page query

--- src/db/subscriptions/recentDocumentQuerySubscription.js
+++ src/db/subscriptions/recentDocumentQuerySubscription.js
@@ -1,11 +1,12 @@
-import { DB_DOCUMENTS_PROJECTION, VIS_DOC_TYPE, DATA_DOC_TYPE } from '../../constants'
+import { DB_DOCUMENTS_PROJECTION, VIS_DOC_TYPE, DATA_DOC_TYPE, TECH_DOC_TYPE } from '../../constants'
 import BaseQuerySubscription from './baseQuerySubscription'
 
 export default () => BaseQuerySubscription(
   {
     $or: [
       { type: VIS_DOC_TYPE, viewCount: { $gte: 20 } },
-      { type: DATA_DOC_TYPE, viewCount: { $gte: 3 } }
+      { type: DATA_DOC_TYPE, viewCount: { $gte: 5 } },
+      { type: TECH_DOC_TYPE, viewCount: { $gte: 5 } }
     ],
     isPrivate: { $ne: true },
     $sort: { viewCount: -1 }
