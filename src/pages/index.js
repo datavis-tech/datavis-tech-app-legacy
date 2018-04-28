09e90fe8918127c463abf8ac7278a6e6fafc3508
shurik <stu.shurik@gmail.com>
2018-04-28 13:59:20 +0000

    [refactor] render home page only from props without subscription
    Closes #391

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,37 +1,51 @@
 import React from 'react'
 import { Container, Header } from 'semantic-ui-react'
-import RecentDocumentQuerySubscription from '../db/subscriptions/recentDocumentQuerySubscription'
-import { serializeDocument } from '../db/serializers'
-import Subscription from '../components/subscription'
 import Page from '../components/page'
 import Layout from '../components/layout'
-import Loader from '../components/loader'
 import Spacer from '../components/spacer'
 import HugeLogo from '../components/hugeLogo'
 import DocumentsList from '../components/documentsList'
 
-// TODO: test
-export default Page(({ user }) => (
-  <Layout title='Datavis.tech' user={user}>
-    <Container text>
-      <Spacer space='60px' />
-      <HugeLogo noLink />
-      <Spacer space='10px' />
-      <Container textAlign='center'>
-        <Header as='h1'>A collaborative data visualization platform.</Header>
+// TODO test
+function Home ({ user, recentDocuments }) {
+  return (
+    <Layout title='Datavis.tech' user={user}>
+      <Container text>
+        <Spacer space='60px' />
+        <HugeLogo noLink />
+        <Spacer space='10px' />
+        <Container textAlign='center'>
+          <Header as='h1'>
+                        A collaborative data visualization platform.
+          </Header>
+        </Container>
+        <Spacer space='50px' />
+        <p style={{ fontSize: '1.5em', textAlign: 'center' }}>
+                    You can use this site to publish datasets and create
+                    visualizations, collaborating with others in real time.
+        </p>
+        <Spacer space='50px' />
       </Container>
-      <Spacer space='50px' />
-      <p style={{fontSize: '1.5em', textAlign: 'center'}}>You can use this site to publish datasets and create visualizations, collaborating with others in real time.</p>
-      <Spacer space='50px' />
-    </Container>
-    <Subscription subscription={RecentDocumentQuerySubscription()} >
-      {
-        ({data: documents, isReady}) => (
-          <Loader ready={isReady}>
-            <DocumentsList documents={(documents || []).map(serializeDocument)} />
-          </Loader>
-        )
-      }
-    </Subscription>
-  </Layout>
-))
+      <DocumentsList documents={recentDocuments} />
+    </Layout>
+  )
+}
+
+Home.getInitialProps = async ({ req }) => {
+
+  let recentDocuments = []
+
+  if (req) {
+    require('isomorphic-fetch')
+    /* global fetch:false */
+    const response = await fetch(
+      'http://localhost:3000/rest/documents/recent'
+    )
+
+    recentDocuments = await response.json()
+  }
+
+  return { recentDocuments }
+}
+
+export default Page(Home)
